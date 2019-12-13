5d0356ada16b21acb718a3437da55805e6b554ef
Shota Amashukeli <shota97@rocket.hpc.ut.ee>
2019-12-13 15:51:34 +0200

    sk biggest

--- sk_md.py
+++ sk_md.py
@@ -16,35 +16,38 @@ import time
 
 
 #filename='blood.csv'
-filename='2dplanes.csv'
-
-n=100
-
-data=pd.read_csv(filename)
-X=data.iloc[:,:-1]
-y=data.iloc[:,-1]
-#f=int(math.log(X.shape[1]+1,2))
+#filename='2dplanes.csv'
+filename='custom_satisfaction.csv'
 
 
-# In[24]:
+ns=[1,2,4,8,16,32]
+for n in ns:
+	data=pd.read_csv(filename)
+	data=data.drop('ID',axis=1)
+	X=data.iloc[:,:-1]
+	y=data.iloc[:,-1]
+	#f=int(math.log(X.shape[1]+1,2))
 
 
-start=time.time()
-clf = RandomForestClassifier(n_estimators=n,random_state=0)
-clf.fit(X, y)
-taken=time.time()-start
-print(taken)
-pd.DataFrame([[filename,taken,n]]).to_csv('rf_sk.txt',mode='a',index=False,header=False)
-
-# In[25]:
+	# In[24]:
 
 
-start=time.time()
-clf = BaggingClassifier(n_estimators=n,random_state=0)
-clf.fit(X, y)
-taken=time.time()-start
-print(taken)
-pd.DataFrame([[filename,taken,n]]).to_csv('bag_sk.txt',mode='a',index=False,header=False)
+	start=time.time()
+	clf = RandomForestClassifier(n_estimators=n,random_state=0)
+	clf.fit(X, y)
+	taken=time.time()-start
+	print(taken)
+	pd.DataFrame([[filename,taken,n]]).to_csv('rf_sk.txt',mode='a',index=False,header=False)
+
+	# In[25]:
+
+
+	start=time.time()
+	clf = BaggingClassifier(n_estimators=n,random_state=0)
+	clf.fit(X, y)
+	taken=time.time()-start
+	print(taken)
+	pd.DataFrame([[filename,taken,n]]).to_csv('bag_sk.txt',mode='a',index=False,header=False)
 
 
 # In[ ]:
